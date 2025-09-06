a748e38eda33e5b61ff0606c1c53e3776fd41fed
Carlos <carlos@local.es>
2025-09-06 16:25:09 +0200

    Cambios 06/09-2

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,10 @@ use App\Http\Controllers\OTPController;
 use App\Http\Middleware\AzureTokenMiddleware;
 use Illuminate\Support\Facades\Route;
 
+Route::get("/test", function () {
+	return "OK!";
+});
+
 Route::middleware(AzureTokenMiddleware::class) -> group(function () {
 	Route::get("/opt/all", [OTPController::class, "newOTP"]);
 
