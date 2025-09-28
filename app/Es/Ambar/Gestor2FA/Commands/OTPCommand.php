b3220cc8b5b9efb7d649de2a0c24f604bc2c1c6a
Carlos <carlos@local.es>
2025-09-28 16:17:08 +0200

    sd

--- app/Es/Ambar/Gestor2FA/Commands/OTPCommand.php
+++ app/Es/Ambar/Gestor2FA/Commands/OTPCommand.php
@@ -8,16 +8,14 @@ class OTPCommand
 {
 	public function newOTP($code, $name): bool
 	{
-		return Process::env(["PATH" => "/snap/bin/2fa"])
-			-> input("$code\n")
+		return Process::input("$code\n")
 			-> run("2fa -add {$name};")
 			-> successful();
 	}
 
 	public static function getOTPCode(string $name): string
 	{
-		$process = Process::env(["PATH" => "/snap/bin/2fa"])
-			-> run("2fa {$name}");
+		$process = Process::run("2fa {$name}");
 
 		return $process -> successful()
 			?  $process -> output()
@@ -25,8 +23,7 @@ class OTPCommand
 	}
 
 	public function getAllOTP() {
-		return Process::env(["PATH" => "/snap/bin/2fa"])
-			-> run("2fa")
+		return Process::run("2fa")
 			-> output();
 	}
 }
